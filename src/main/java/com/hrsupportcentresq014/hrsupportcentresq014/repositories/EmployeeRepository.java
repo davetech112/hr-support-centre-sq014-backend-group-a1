83cd0af1caf743af07e05991d8a22f59839119b2
Maximillian <maximillian.ifeanyi@decagon.dev>
2023-05-15 07:51:30 +0100

    Included a data seeder.

---
+++ src/main/java/com/hrsupportcentresq014/hrsupportcentresq014/repositories/EmployeeRepository.java
@@ -0,0 +1,16 @@
+package com.hrsupportcentresq014.hrsupportcentresq014.repositories;
+
+import com.hrsupportcentresq014.hrsupportcentresq014.entities.Employee;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface EmployeeRepository extends MongoRepository<Employee, String> {
+
+
+//    Optional<Employee> findEmployeeByEmail(String email);
+
+    Optional<Employee> findByEmail(String username);
+}
