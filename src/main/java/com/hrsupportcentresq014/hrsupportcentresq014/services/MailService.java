056d71cf0ec003e8549240c9df93b3fdd7dc2987
laurd-spada <82173669+laurd-spada@users.noreply.github.com>
2023-05-16 14:46:39 +0100

    messaging implementations and handlers

--- src/main/java/com/hrsupportcentresq014/hrsupportcentresq014/services/MailService.java
+++ src/main/java/com/hrsupportcentresq014/hrsupportcentresq014/services/MailService.java
@@ -3,7 +3,7 @@ package com.hrsupportcentresq014.hrsupportcentresq014.services;
 import jakarta.mail.MessagingException;
 
 public interface MailService {
-    void sendMailTest(String fullName, String subject, String email, String text,String messageSubject) throws MessagingException;
-    void sendAccountActivation(String firstname, String lastname, String email, String activated_password) throws MessagingException;
-    void passwordReset(String fullName, String resetUrl, String altEmail) throws MessagingException;
+    void sendMailTest(String senderEmail,String messageSubject, String messageBody) throws MessagingException;
+    void sendAccountActivation(String receiverEmail, String activationUrl) throws MessagingException;
+    void passwordReset(String resetUrl, String receiverEmail) throws MessagingException;
 }
