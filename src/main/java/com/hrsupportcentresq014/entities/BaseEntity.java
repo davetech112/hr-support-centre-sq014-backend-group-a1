6abec515553d117c88ae85f970f191fc189063bf
laurd-spada <daniel57_real@yahoo.com>
2023-05-18 11:32:41 +0100

    rectifying error

--- src/main/java/com/hrsupportcentresq014/entities/BaseEntity.java
+++ src/main/java/com/hrsupportcentresq014/entities/BaseEntity.java
@@ -1,7 +1,6 @@
 package com.hrsupportcentresq014.entities;
 
 
-import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
@@ -11,6 +10,5 @@ public class BaseEntity {
     @Id
     private String id;
 
-    @CreationTimestamp
     private LocalDateTime createdOn;
 }
