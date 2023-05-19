0ba158c7d9b95768499b5de99ea7fae50e525763
Chris Ogwuche <ogwuchechris@gmail.com>
2023-05-19 02:03:17 +0100

    Update BaseEntity.java

--- src/main/java/com/hrsupportcentresq014/entities/BaseEntity.java
+++ src/main/java/com/hrsupportcentresq014/entities/BaseEntity.java
@@ -1,15 +1,11 @@
 package com.hrsupportcentresq014.entities;
 
-import org.hibernate.annotations.CreationTimestamp;
 import org.springframework.data.annotation.Id;
 
 import java.time.LocalDateTime;
 
 public class BaseEntity {
-
     @Id
     private String id;
-
-    @CreationTimestamp
-    private LocalDateTime createdOn;
+    private LocalDateTime createdOn = LocalDateTime.now();
 }
