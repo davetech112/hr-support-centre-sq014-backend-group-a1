8a65c0ad33868d2df9d7b7fb05a2c2292216d65e
Maximillian <agwaigbomaximillian@gmail.com>
2023-06-06 13:35:52 +0100

    Add Admin to add roles permissions

--- src/main/java/com/hrsupportcentresq014/services/RoleService.java
+++ src/main/java/com/hrsupportcentresq014/services/RoleService.java
@@ -7,5 +7,5 @@ import com.hrsupportcentresq014.dtos.response.AddRoleResponse;
 public interface RoleService {
 
     AddRoleResponse addRoles(AddRoleRequest addRole);
-
+    AddRoleResponse addPermission(AddPermissionRequest request, String role_id);
 }
